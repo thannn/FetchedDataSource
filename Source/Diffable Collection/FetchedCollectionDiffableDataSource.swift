a89c159fbfb6cd038605ac5430ee271ca5bce1b5
Jonathan Provo <provo.jonathan@appwise.be>
2022-09-19 23:27:53 +0200

    Ensure data integrity of the snapshot

--- Source/Diffable Collection/FetchedCollectionDiffableDataSource.swift
+++ Source/Diffable Collection/FetchedCollectionDiffableDataSource.swift
@@ -19,6 +19,7 @@ public final class FetchedCollectionDiffableDataSource: NSObject, NSFetchedResul
 
 	private let controller: NSFetchedResultsController<NSFetchRequestResult>
 	private let dataSource: FetchedDiffableDataSource
+	private lazy var internalSnapshot: NSDiffableDataSourceSnapshot<FetchedDiffableSection, FetchedDiffableItem> = .init() // actual, non-modifiable snapshot to maintain data integrity
 	private weak var delegate: FetchedCollectionDiffableDataSourceDelegate?
 
 	// MARK: - Lifecycle
@@ -60,30 +61,30 @@ public final class FetchedCollectionDiffableDataSource: NSObject, NSFetchedResul
 
 		let snapshotWithPermanentIDs = obtainPermanentIDs(for: snapshot, in: controller.managedObjectContext)
 		let typedSnapshot = snapshotWithPermanentIDs as NSDiffableDataSourceSnapshot<String, NSManagedObject>
-		var snapshot = dataSource.snapshot()
 		typedSnapshot.sectionIdentifiers.forEach { sectionIdentifier in
 			let section: FetchedDiffableSection = .init(identifier: sectionIdentifier)
-			snapshot.deleteSections([section])
+			internalSnapshot.deleteSections([section])
 			let items: [FetchedDiffableItem] = typedSnapshot.itemIdentifiers(inSection: sectionIdentifier).map { .init(item: $0) }
-			snapshot.appendSections([section])
-			snapshot.appendItems(items, toSection: section)
+			internalSnapshot.appendSections([section])
+			internalSnapshot.appendItems(items, toSection: section)
 		}
 
-		contentChangeWillBeApplied(snapshot: &snapshot)
+		var externalSnapshot = internalSnapshot // duplicate, modifiable snapshot to be displayed
+		contentChangeWillBeApplied(snapshot: &externalSnapshot)
 
 		DispatchQueue.main.async {
 			// iOS bug: reloaded items are not included in the snapshot when `animatingDifferences` is `true`
 			// workaround: applying the snapshot with `animatingDifferences` set to `false` reloads the items properly
 			if self.isAnimatingDifferences {
-				self.dataSource.apply(snapshot, animatingDifferences: true) { [weak self] in
-					self?.dataSource.apply(snapshot, animatingDifferences: false) {
+				self.dataSource.apply(externalSnapshot, animatingDifferences: true) { [weak self] in
+					self?.dataSource.apply(externalSnapshot, animatingDifferences: false) {
 						DispatchQueue.main.async { // dispatch async required to avoid deadlock in case the delegate inititates another `apply()`
 							self?.contentDidChange()
 						}
 					}
 				}
 			} else {
-				self.dataSource.apply(snapshot, animatingDifferences: false) { [weak self] in
+				self.dataSource.apply(externalSnapshot, animatingDifferences: false) { [weak self] in
 					DispatchQueue.main.async { // dispatch async required to avoid deadlock in case the delegate inititates another `apply()`
 						self?.contentDidChange()
 					}
